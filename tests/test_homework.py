521c9d8c887c9f2a168f141e677ceed8e0915fa3
David Esteban Cartagena Mejía <160861472+dcartagena2001@users.noreply.github.com>
2025-05-24 12:28:34 -0500

    resultados

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -9,12 +9,12 @@ warnings.filterwarnings("ignore")
 
 def test_01():
 
+
     # Test if the homework script runs without errors
     try:
         subprocess.run(
             [
-                "source",
-                "run.sh",
+                "mlflow run https://github.com/jdvelasq/mlflow-wine-quality.git -e knn -P n_neighbors=5"
             ],
             check=True,
         )
